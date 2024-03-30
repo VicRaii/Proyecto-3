df7b13a38b0acb0b53a5fe9ecfeab3e45c88bd4e
vimarupon@hotmail.com <vimarupon@gmail.com>
2024-03-30 13:22:29 +0100

    fifth commit

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
-import {
-  gridGalleryConfig,
-  searchImages,
-  showMoreImages,
-} from "./src/components/Gallery/gallery";
+// import {
+//   gridGalleryConfig,
+//   searchImages,
+//   showMoreImages,
+// } from "./src/components/Gallery/gallery";
 
-import {
-  buttonContainerConfig,
-  logoContainerConfig,
-  searchBarContainerConfig,
-} from "./src/components/Header/header";
+// import {
+//   buttonContainerConfig,
+//   logoContainerConfig,
+//   searchBarContainerConfig,
+// } from "./src/components/Header/header";
 
+import { gridGalleryConfig } from "./src/components/Gallery/gallery";
+import { headerConfig } from "./src/components/Header/header";
 import "./style.css";
 
-logoContainerConfig();
-searchBarContainerConfig();
-buttonContainerConfig();
+// logoContainerConfig();
+// searchBarContainerConfig();
+// buttonContainerConfig();
 
-searchImages();
-showMoreImages();
+// searchImages();
+
+// showMoreImages();
+
+// gridGalleryConfig();
+
+headerConfig();
 gridGalleryConfig();
--- src/components/Gallery/gallery.js
+++ src/components/Gallery/gallery.js
@@ -1,82 +1,130 @@
+// import "./gallery.css";
+
+// let keyWord = "";
+// let page;
+
+// export const searchImages = async () => {
+//   const searchBar = document.querySelector("input");
+//   const gridGallery = document.querySelector(".grid-gallery");
+
+//   const accesKey = "woWM4AxFrJe9NXSiYDFuBkUb8l3aFClA12t438zVkIg";
+//   const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyWord}&client_id=${accesKey}&per_page=12`;
+
+//   keyWord = searchBar.value;
+
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log(data);
+
+//     if (page === 1) {
+//       gridGallery.innerHTML = "";
+//     }
+
+//     const images = data.results;
+
+//     images.map((result) => {
+//       const image = document.createElement("img");
+//       const imageLink = document.createElement("a");
+//       const imageDescription = document.createElement("p");
+
+//       image.src = result.urls.small;
+//       imageLink.href = result.links.html;
+//       imageLink.target = "_blank";
+//       imageDescription.textContent = result.alt_description;
+
+//       imageLink.appendChild(image);
+//       gridGallery.appendChild(imageLink);
+//       imageLink.appendChild(imageDescription);
+//     });
+//   } catch (error) {
+//     console.error("Error fetching images:", error);
+//   }
+// };
+
+// export const showMoreImages = () => {
+//   const showMoreButton = document.querySelector("#show-more");
+//   const searchBar = document.querySelector("input");
+
+//   showMoreButton.textContent = "Show More";
+
+//   showMoreButton.addEventListener("click", async () => {
+//     page++;
+//     await searchImages();
+//   });
+
+//   searchBar.addEventListener("input", searchImages);
+// };
+
+// export const gridGalleryConfig = () => {
+//   const searchBarContainer = document.querySelector(".search-bar-container");
+//   const showMoreButton = document.querySelector("#show-more");
+
+//   searchBarContainer.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     searchImages();
+//   });
+// };
+
 import "./gallery.css";
-
-let keyWord = "";
-let page;
-
-export const searchImages = async () => {
-  const searchBar = document.querySelector("input");
-  const gridGallery = document.querySelector(".grid-gallery");
-
-  const accesKey = "woWM4AxFrJe9NXSiYDFuBkUb8l3aFClA12t438zVkIg";
-
-  keyWord = searchBar.value;
-
-  try {
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyWord}&client_id=${accesKey}&per_page=12`;
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-
-    if (page === 1) {
-      gridGallery.innerHTML = "";
-    }
-
-    const images = data.results;
-
-    images.forEach((result) => {
-      const image = document.createElement("img");
-      const imageLink = document.createElement("a");
-      const imageDescription = document.createElement("p");
-
-      image.src = result.urls.small;
-      imageLink.href = result.links.html;
-      imageLink.target = "_blank";
-      imageDescription.textContent = result.alt_description;
-
-      imageLink.appendChild(image);
-      gridGallery.appendChild(imageLink);
-      imageLink.appendChild(imageDescription);
-    });
-  } catch (error) {
-    console.error("Error fetching images:", error);
-  }
-};
-
-export const showMoreImages = () => {
-  const showMoreButton = document.querySelector("#show-more");
-  const searchBar = document.querySelector("input");
-
-  showMoreButton.textContent = "Show More";
-
-  showMoreButton.addEventListener("click", async () => {
-    page++;
-    await searchImages();
-  });
-
-  searchBar.addEventListener("input", searchImages);
-};
 
 export const gridGalleryConfig = () => {
   const searchBarContainer = document.querySelector(".search-bar-container");
-  const refreshButton = document.querySelector("#refresh-button");
+  const searchBar = document.querySelector("input");
+  const gridGallery = document.querySelector(".grid-gallery");
+  const showMoreButton = document.querySelector("#show-more");
 
-  page = Math.floor(Math.random() * 155) + 1;
+  let keyWord = "";
+  let page = 1;
+  const accesKey = "woWM4AxFrJe9NXSiYDFuBkUb8l3aFClA12t438zVkIg";
 
-  searchBarContainer.addEventListener("submit", async (e) => {
+  async function searchImages() {
+    keyWord = searchBar.value;
+
+    try {
+      const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyWord}&client_id=${accesKey}&per_page=12`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+
+      if (page === 1) {
+        gridGallery.innerHTML = "";
+      }
+
+      const images = data.results;
+
+      images.forEach((result) => {
+        const image = document.createElement("img");
+        const imageLink = document.createElement("a");
+        const imageDescription = document.createElement("p");
+
+        image.src = result.urls.small;
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageDescription.textContent = result.alt_description;
+
+        imageLink.appendChild(image);
+        gridGallery.appendChild(imageLink);
+        imageLink.appendChild(imageDescription);
+      });
+    } catch (error) {
+      console.error("Error fetching images:", error);
+    }
+  }
+
+  window.addEventListener("load", searchImages);
+
+  searchBarContainer.addEventListener("submit", (e) => {
     e.preventDefault();
-    await searchImages();
+    searchImages();
   });
 
-  refreshButton.textContent = "Refresh";
+  showMoreButton.textContent = "Show More";
 
-  refreshButton.addEventListener("click", async () => {
-    page = Math.floor(Math.random() * 155) + 1;
-    await searchImages();
+  showMoreButton.addEventListener("click", () => {
+    page++;
+    searchImages();
   });
 
-  // Llamar a searchImages al cargar la página
-  window.addEventListener("load", async () => {
-    await searchImages();
-    showMoreImages();
-  });
+  searchBar.addEventListener("input", searchImages);
 };
