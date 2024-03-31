e395ad599aab67a4175c87349b086d8480d856e0
vimarupon@hotmail.com <vimarupon@gmail.com>
2024-03-31 19:27:57 +0200

    7 commit

--- src/components/Gallery/gallery.js
+++ src/components/Gallery/gallery.js
@@ -1,81 +1,13 @@
-// import "./gallery.css";
-
-// let keyWord = "";
-// let page;
-
-// export const searchImages = async () => {
-//   const searchBar = document.querySelector("input");
-//   const gridGallery = document.querySelector(".grid-gallery");
-
-//   const accesKey = "woWM4AxFrJe9NXSiYDFuBkUb8l3aFClA12t438zVkIg";
-//   const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyWord}&client_id=${accesKey}&per_page=12`;
-
-//   keyWord = searchBar.value;
-
-//   try {
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     console.log(data);
-
-//     if (page === 1) {
-//       gridGallery.innerHTML = "";
-//     }
-
-//     const images = data.results;
-
-//     images.map((result) => {
-//       const image = document.createElement("img");
-//       const imageLink = document.createElement("a");
-//       const imageDescription = document.createElement("p");
-
-//       image.src = result.urls.small;
-//       imageLink.href = result.links.html;
-//       imageLink.target = "_blank";
-//       imageDescription.textContent = result.alt_description;
-
-//       imageLink.appendChild(image);
-//       gridGallery.appendChild(imageLink);
-//       imageLink.appendChild(imageDescription);
-//     });
-//   } catch (error) {
-//     console.error("Error fetching images:", error);
-//   }
-// };
-
-// export const showMoreImages = () => {
-//   const showMoreButton = document.querySelector("#show-more");
-//   const searchBar = document.querySelector("input");
-
-//   showMoreButton.textContent = "Show More";
-
-//   showMoreButton.addEventListener("click", async () => {
-//     page++;
-//     await searchImages();
-//   });
-
-//   searchBar.addEventListener("input", searchImages);
-// };
-
-// export const gridGalleryConfig = () => {
-//   const searchBarContainer = document.querySelector(".search-bar-container");
-//   const showMoreButton = document.querySelector("#show-more");
-
-//   searchBarContainer.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     searchImages();
-//   });
-// };
-
 import "./gallery.css";
+
+let keyWord = "";
+let page = 1;
 
 export const gridGalleryConfig = () => {
   const searchBarContainer = document.querySelector(".search-bar-container");
   const searchBar = document.querySelector("input");
   const gridGallery = document.querySelector(".grid-gallery");
-  const showMoreButton = document.querySelector("#show-more");
 
-  let keyWord = "";
-  let page = 1;
   const accesKey = "woWM4AxFrJe9NXSiYDFuBkUb8l3aFClA12t438zVkIg";
 
   async function searchImages() {
@@ -112,22 +44,42 @@ export const gridGalleryConfig = () => {
     }
   }
 
-  // window.addEventListener("load", () => {
-  //   console.log("La ventana se ha cargado completamente.");
-  //   searchImages();
-  // });
-
   searchBarContainer.addEventListener("submit", (e) => {
     e.preventDefault();
     searchImages();
   });
 
-  showMoreButton.textContent = "Show More";
+  searchBar.addEventListener("input", searchImages);
 
-  showMoreButton.addEventListener("click", () => {
-    page++;
-    searchImages();
+  const showMoreButtonConfig = () => {
+    const showMoreButton = document.querySelector("#show-more");
+
+    showMoreButton.textContent = "Show More";
+
+    showMoreButton.addEventListener("click", () => {
+      page++;
+      searchImages();
+    });
+  };
+
+  showMoreButtonConfig();
+};
+
+const upArrowConfig = () => {
+  const upArrow = document.querySelector("#up-arrow");
+  const upArrowImage = document.createElement("img");
+
+  upArrow.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   });
 
-  searchBar.addEventListener("input", searchImages);
+  upArrowImage.src = "/assets/up-arrow.png";
+  upArrowImage.alt = "Go up Arrow";
+
+  upArrow.appendChild(upArrowImage);
 };
+
+upArrowConfig();
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,71 +1,7 @@
-// import "./header.css";
-
-// export const logoContainerConfig = () => {
-//   const logoContainer = document.querySelector(".logo-container");
-
-//   const createLogoLink = document.createElement("a");
-//   const createLogoImage = document.createElement("img");
-
-//   createLogoLink.href = "#";
-//   createLogoLink.target = "_blank";
-//   createLogoLink.classList.add("pinterest-logo-link");
-//   createLogoImage.src = "./assets/pinterest-big-Logo.png";
-//   createLogoImage.alt = "pinterest-logo";
-
-//   logoContainer.appendChild(createLogoLink);
-//   createLogoLink.appendChild(createLogoImage);
-// };
-
-// export const searchBarContainerConfig = () => {
-//   const searchBarContainer = document.querySelector(".search-bar-container");
-//   const searchBar = document.createElement("input");
-//   const searchButton = document.createElement("button");
-
-//   searchBar.type = "search";
-//   searchBar.classList.add("search-bar");
-//   searchBar.id = "search-box";
-//   searchBar.placeholder = "What are you looking for?";
-//   searchButton.textContent = "🔍";
-
-//   searchBarContainer.appendChild(searchBar);
-//   searchBarContainer.appendChild(searchButton);
-// };
-
-// export const buttonContainerConfig = () => {
-//   const gridGallery = document.querySelector(".grid-gallery");
-//   const buttonContainer = document.querySelector(".button-container");
-//   const refreshButton = document.createElement("button");
-
-//   refreshButton.type = "button";
-//   refreshButton.classList.add("refresh-button");
-//   refreshButton.textContent = "Refresh";
-
-//   buttonContainer.appendChild(refreshButton);
-
-//   refreshButton.addEventListener("click", () => {
-//     gridGallery.innerHTML = "";
-//   });
-// };
-
+import { gridGalleryConfig } from "../Gallery/gallery";
 import "./header.css";
 
 export const headerConfig = () => {
-  // const logoContainerConfig = () => {
-  //   const logoContainer = document.querySelector(".logo-container");
-
-  //   const createLogoLink = document.createElement("a");
-  //   const createLogoImage = document.createElement("img");
-
-  //   createLogoLink.href = "#";
-  //   createLogoLink.target = "_blank";
-  //   createLogoLink.classList.add("pinterest-logo-link");
-  //   createLogoImage.src = "./public/assets/pinterest-big-logo.png";
-  //   createLogoImage.alt = "pinterest-logo";
-
-  //   logoContainer.appendChild(createLogoLink);
-  //   createLogoLink.appendChild(createLogoImage);
-  // };
-
   const logoContainerConfig = () => {
     const logoContainer = document.querySelector(".logo-container");
 
@@ -79,16 +15,16 @@ export const headerConfig = () => {
   const searchBarContainerConfig = () => {
     const searchBarContainer = document.querySelector(".search-bar-container");
     const searchBar = document.createElement("input");
-    // const searchButton = document.createElement("button");
+    const searchButton = document.createElement("button");
 
     searchBar.type = "search";
     searchBar.classList.add("search-bar");
     searchBar.id = "search-box";
     searchBar.placeholder = "What are you looking for?";
-    // searchButton.textContent = "🔍";
+    searchButton.textContent = "Search";
 
     searchBarContainer.appendChild(searchBar);
-    // searchBarContainer.appendChild(searchButton);
+    searchBarContainer.appendChild(searchButton);
   };
 
   const buttonContainerConfig = () => {
@@ -110,4 +46,5 @@ export const headerConfig = () => {
   searchBarContainerConfig();
   logoContainerConfig();
   buttonContainerConfig();
+  gridGalleryConfig();
 };
